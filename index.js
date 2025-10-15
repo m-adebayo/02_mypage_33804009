51ee27ebf69d2e46b66622347188d7b8827e44e3
madebayo <malachi.adebayo0@gmail.com>
2025-10-15 09:23:35 +0100

    Troubleshooting Change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function(req, res) {
     res.end(`<!DOCTYPE html>
 <html>
 <head>
-    <title>My Web Page</title>
+    <title>Web Page</title>
 </head>
 <body>
     <h1>Welcome to My Page</h1>
