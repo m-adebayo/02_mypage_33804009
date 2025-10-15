53b858e69a6bc3baa06d031f7c946c536d38d653
m-adebayo <malachi.adebayo0@gmail.com>
2025-10-15 16:16:30 +0100

    Changes to test VM

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ http.createServer(function(req, res) {
 <body>
     <h1>Welcome to My Page</h1>
     <h2>About Me</h2>
-    <p> My name is Malachi. This is for the Dynamic Web applications module.</p>
+    <p> My name is Malachi. This is for the Dynamic Web applications module. This change was made after deployment to the VM, to test whether changes are visible on both sides.</p>
 </body>
 </html>`
     );
